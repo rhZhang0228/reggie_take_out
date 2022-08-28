33502afff7e8ed1cf541ffb7c1130974fa1afc8c
Ultraman Zero <1809674320@qq.com>
2022-08-28 03:45:49 +0000

    删除文件 src

--- src/main/java/com/itheima/service/OrdersService.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.entity.Orders;
-
-public interface OrdersService extends IService<Orders> {
-    public void submit(Orders orders);
-}
--- src/main/java/com/itheima/service/SetmealDishService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.entity.SetmealDish;
-
-public interface SetmealDishService extends IService<SetmealDish> {
-}
